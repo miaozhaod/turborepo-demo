187163ac29ddcc6ae9915dc071df8375967f2fb7
Miao Zhao <miao_zhao@yahoo.com>
2022-10-02 22:31:54 +1000

    Update to remove lint errors

--- apps/next-next-app/src/pages/_app.tsx
+++ apps/next-next-app/src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import '../../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Component {...pageProps} />
+)
 
 export default MyApp
